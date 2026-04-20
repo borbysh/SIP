3db1d400973e25a33e40074a4c367262851365d0
borbysh <56835947+borbysh@users.noreply.github.com>
2026-04-20 19:14:33 +0300

    Refactor VTK function names for consistency

--- vtk_output.h
+++ vtk_output.h
@@ -1,13 +1,20 @@
 #ifndef VTK_OUTPUT_H
 #define VTK_OUTPUT_H
 
+// Function to write VTK header information
+void write_vtk_header(FILE *file, const char *title, int nx, int ny);
+
+// Function to write data to VTK file
+void write_data(FILE *file, const double *data, int num_points);
+
 // Function to generate VTK output files
-void generateVTKOutput(const char* filename, const double* temperatureField, int fieldSize);
+void generate_vtk_output(const char *filename, const double *numerical_solution, 
+                        const double *analytical_solution, int nx, int ny);
 
 // Function to initialize VTK settings
-void initializeVTKVisualization();
+void initialize_vtk_visualization(void);
 
 // Function to finalize VTK visualization
-void finalizeVTKVisualization();
+void finalize_vtk_visualization(void);
 
 #endif // VTK_OUTPUT_H
